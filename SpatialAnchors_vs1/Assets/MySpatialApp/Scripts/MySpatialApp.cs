cf97aa708d914fdfc6e2dad17039e46abbfa85f1
Corinna Braun <corinna.braun@meapuna.de>
2019-10-07 15:14:56 +0200

    Unity Project eingefügt

---
+++ SpatialAnchors_vs1/Assets/MySpatialApp/Scripts/MySpatialApp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.Azure.SpatialAnchors {
+
+    public class MySpatialApp : MonoBehaviour {
+        CloudSpatialAnchorSession cloudSession;
+        // Start is called before the first frame update
+        void Start () {
+            this.cloudSession = new CloudSpatialAnchorSession ();
+            Debug.Log (cloudSession);
+            this.cloudSession.Configuration.AccountKey = @"MyAccountKey";
+            this.cloudSession.Configuration.AccessToken = @"MyAccessToken";
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+    }
+}
